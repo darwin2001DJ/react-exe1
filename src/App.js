42909ab4be5569eedc766a66fb2e6715878c89f7
Your Name <darwinmuthu450@gmail.com>
2020-11-03 11:29:16 +0530

    app init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const kitchenItemsPurchased = [
+    "Paper Towel Holder",
+    "Cups",
+    "Drawer Organizers",
+    "Water Filtration",
+    "Placemats",
+    "Cutting Board",
+    "Shelf Liners",
+    "Placemats",
+    "Cutting Board",
+    "Shelf Liners",
+  ];
+  const utilityItemsPurchased = [
+    "Light Bulbs",
+    "Extension Cords",
+    "Batteries",
+    "First Aid Kit",
+    "Flashlights",
+    "Candles",
+    "Matches",
+  ];
+  const homeDecortemsPurchased = [
+    "Toss Pillows",
+    "Slipcovers",
+    "Picture Frames",
+    "Wall Art",
+    "Door Mats",
+    "Lighting",
+    "Wall Hanging Kits",
+  ];
+  const bedroomItemsPurchased = [
+    "Mattress Pad",
+    "Mattress Protector",
+    "Air Mattress",
+    "Pillow Protectors",
+    "Pillows",
+    "Blankets",
+    "Quilts",
+  ];
 }
 
 export default App;
